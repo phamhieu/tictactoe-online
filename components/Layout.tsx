734264ec5ac320e1ae6c41a51f872150aa6463d3
phamhieu <phamhieu1998@gmail.com>
2021-08-03 17:20:13 +0700

    feat: add game board

---
+++ components/Layout.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import Head from 'next/head'
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div className="min-h-screen pt-16 pb-12 flex flex-col bg-white">
+      <Head>
+        <title>TicTacToe online</title>
+        <meta name="description" content="Tic tac toe online; play the classic game with others." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+        {children}
+      </main>
+      <footer className="flex-shrink-0 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+        <nav className="flex justify-center space-x-4">
+          <a href="#" className="text-sm font-medium text-gray-500 hover:text-gray-600">
+            Twitter
+          </a>
+          <span className="inline-block border-l border-gray-300" aria-hidden="true" />
+          <a href="#" className="text-sm font-medium text-gray-500 hover:text-gray-600">
+            Github
+          </a>
+        </nav>
+      </footer>
+    </div>
+  )
+}
+export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,26 @@
 import * as React from 'react'
-import Head from 'next/head'
 import toast from 'react-hot-toast'
 import { supabase } from '../lib/supabaseClient'
 import Loading from '../components/Loading'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className="min-h-screen pt-16 pb-12 flex flex-col bg-white">
-      <Head>
-        <title>TicTacToe online</title>
-        <meta name="description" content="Tic tac toe online; play the classic game with others." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex-shrink-0 flex justify-center">
-          <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
-            Tic Tac Toe online
-          </h1>
+    <Layout>
+      <div className="flex-shrink-0 flex justify-center">
+        <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+          Tic Tac Toe online
+        </h1>
+      </div>
+      <div className="mx-auto py-24">
+        <div className="text-center">
+          <p className="mt-2 text-base text-gray-500">
+            Sign in with magic link to start playing with others
+          </p>
+          <MagicLinkForm />
         </div>
-        <div className="mx-auto py-24">
-          <div className="text-center">
-            <p className="mt-2 text-base text-gray-500">
-              Sign in with magic link to start playing with others
-            </p>
-            <MagicLinkForm />
-          </div>
-        </div>
-      </main>
-      <footer className="flex-shrink-0 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-        <nav className="flex justify-center space-x-4">
-          <a href="#" className="text-sm font-medium text-gray-500 hover:text-gray-600">
-            Twitter
-          </a>
-          <span className="inline-block border-l border-gray-300" aria-hidden="true" />
-          <a href="#" className="text-sm font-medium text-gray-500 hover:text-gray-600">
-            Github
-          </a>
-        </nav>
-      </footer>
-    </div>
+      </div>
+    </Layout>
   )
 }
 
