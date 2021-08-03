70ae2c52c1f751eaddd30adf5b44cacc3850f1ff
phamhieu <phamhieu1998@gmail.com>
2021-08-03 16:52:53 +0700

    feat: send magic link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
 import '../styles/globals.css'
+import dynamic from 'next/dynamic'
+import { Toaster } from 'react-hot-toast'
 import type { AppProps } from 'next/app'
 
+const PortalRootWithNoSSR = dynamic(
+  (() => import('@radix-ui/react-portal').then((portal) => portal.Root)) as any,
+  { ssr: false }
+)
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <PortalToast />
+    </>
+  )
 }
 export default MyApp
+
+const PortalToast = () => (
+  <PortalRootWithNoSSR>
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        className: 'font-medium',
+        style: {
+          padding: '8px',
+          paddingLeft: '16px',
+          paddingRight: '16px',
+          fontSize: '0.875rem',
+        },
+      }}
+    />
+  </PortalRootWithNoSSR>
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
+import * as React from 'react'
 import Head from 'next/head'
+import toast from 'react-hot-toast'
+import { supabase } from '../lib/supabaseClient'
+import Loading from '../components/Loading'
 
 export default function Home() {
   return (
     <div className="min-h-screen pt-16 pb-12 flex flex-col bg-white">
       <Head>
         <title>TicTacToe online</title>
-        <meta
-          name="description"
-          content="Tic tac toe online; play the classic game with your friends."
-        />
+        <meta name="description" content="Tic tac toe online; play the classic game with others." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,29 +18,12 @@ export default function Home() {
             Tic Tac Toe online
           </h1>
         </div>
-        <div className="max-w-lg mx-auto py-24">
+        <div className="mx-auto py-24">
           <div className="text-center">
             <p className="mt-2 text-base text-gray-500">
               Sign in with magic link to start playing with others
             </p>
-            <form action="#" className="mt-6 flex">
-              <label htmlFor="email" className="sr-only">
-                Email address
-              </label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                className="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                placeholder="Enter an email"
-              />
-              <button
-                type="submit"
-                className="ml-4 flex-shrink-0 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Send magic link
-              </button>
-            </form>
+            <MagicLinkForm />
           </div>
         </div>
       </main>
@@ -55,5 +39,51 @@ export default function Home() {
         </nav>
       </footer>
     </div>
+  )
+}
+
+const MagicLinkForm: React.FC = () => {
+  const [loading, setLoading] = React.useState(false)
+  const [email, setEmail] = React.useState('')
+
+  const handleLogin = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    try {
+      setLoading(true)
+      const { error } = await supabase.auth.signIn({ email })
+      if (error) throw error
+      toast.success('Check your email for the login link!')
+    } catch (error) {
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <form action="#" className="mt-6 flex flex-col">
+      <div className="w-full">
+        <label htmlFor="email" className="sr-only">
+          Email address
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          className="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          placeholder="Enter an email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        className="mt-4 inline-flex justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        onClick={handleLogin}
+      >
+        {loading && <Loading />}
+        Send magic link
+      </button>
+    </form>
   )
 }
