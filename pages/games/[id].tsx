fc170f088deff1cdf997572bcba0eea3ab92ca27
phamhieu <phamhieu1998@gmail.com>
2021-08-06 19:05:02 +0700

    fix: gamemove feeder

--- pages/games/[id].tsx
+++ pages/games/[id].tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { useRouter } from 'next/router'
 import { observer, useLocalObservable } from 'mobx-react-lite'
-import { CheckIcon, ThumbUpIcon, UserIcon } from '@heroicons/react/solid'
+import { CheckIcon, ThumbUpIcon, UserIcon, ChevronUpIcon } from '@heroicons/react/solid'
 import Layout from '../../components/Layout/Layout'
 import Board from '../../components/Game/Board'
 import ProfileHeading from '../../components/Layout/ProfileHeading'
@@ -9,7 +9,7 @@ import { joinClassNames } from '../../lib/helper'
 import { StoreContext } from '../../lib/store'
 import GameStore, { IGameStore } from '../../lib/gameStore'
 import { supabase } from '../../lib/supabaseClient'
-import { runInAction } from 'mobx'
+import dayjs from 'dayjs'
 
 export const GameStoreContext = React.createContext<IGameStore>(undefined!)
 
@@ -106,48 +106,61 @@ const timeline = [
     iconBackground: 'bg-green-500',
   },
 ]
-const GameMoveFeed: React.FC = () => {
+const GameMoveFeed: React.FC = observer(() => {
+  const _gameStore = React.useContext(GameStoreContext)
   return (
     <div className="flow-root">
       <ul className="-mb-8">
-        {timeline.map((event, eventIdx) => (
-          <li key={event.id}>
-            <div className="relative pb-8">
-              {eventIdx !== timeline.length - 1 ? (
-                <span
-                  className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                  aria-hidden="true"
-                />
-              ) : null}
-              <div className="relative flex space-x-3">
-                <div>
-                  <span
-                    className={joinClassNames(
-                      event.iconBackground,
-                      'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'
-                    )}
-                  >
-                    <event.icon className="h-5 w-5 text-white" aria-hidden="true" />
-                  </span>
-                </div>
-                <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                  <div>
-                    <p className="text-sm text-gray-500">
-                      {event.content}{' '}
-                      <a href={event.href} className="font-medium text-gray-900">
-                        {event.target}
-                      </a>
-                    </p>
-                  </div>
-                  <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                    <time dateTime={event.datetime}>{event.date}</time>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
+        {_gameStore.gameMoves.map((move, idx) => (
+          <GameMoveFeedItem
+            key={move.id}
+            idx={idx}
+            value={move.value}
+            position={move.position}
+            insertedAt={move.inserted_at}
+          />
         ))}
       </ul>
     </div>
   )
+})
+
+type GameMoveFeedItem = {
+  idx: number
+  value: string
+  position: number
+  insertedAt: string
 }
+const GameMoveFeedItem: React.FC<GameMoveFeedItem> = observer(
+  ({ idx, value, position, insertedAt }) => {
+    const _gameStore = React.useContext(GameStoreContext)
+    const date = dayjs(insertedAt)
+    return (
+      <li>
+        <div className="relative pb-8">
+          {idx !== _gameStore.gameMoves.length - 1 ? (
+            <span
+              className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
+              aria-hidden="true"
+            />
+          ) : null}
+          <div className="relative flex space-x-3">
+            <div>
+              <span className="h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white bg-blue-500">
+                <ChevronUpIcon className="h-5 w-5 text-white" aria-hidden="true" />
+              </span>
+            </div>
+            <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+              <div>
+                <p className="text-sm text-gray-500">{`Add ${value} to position ${position}`}</p>
+              </div>
+              <div className="text-right text-sm whitespace-nowrap text-gray-500">
+                {date.format('DD-MM-YYYY HH:mm:ss')}
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    )
+  }
+)
